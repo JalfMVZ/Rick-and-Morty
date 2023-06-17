c2d93b793db945b11d3dad96a126f97bfec4fdb1
JalfMVZ <velascojosealfredo1@gmail.com>
2023-06-17 01:09:59 -0500

    login css

--- src/components/Form/Forms.jsx
+++ src/components/Form/Forms.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Validations from "./Validations";
+import style from './Forms.module.css';
 
 export default function Form() {
   const [userData, setUserData] = useState({
@@ -34,36 +35,48 @@ export default function Form() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="email">Email</label>
-      <input
-        type="email"
-        name="email"
-        value={userData.email}
-        onChange={handleChange}
-      />
-      <span>{errors.email && errors.email}</span>
-      <hr />
+    <div className={style.forms_container}>
+      <div className={style.form_card}>
+        <form onSubmit={handleSubmit}>
+          <div className={style.input_container}>
+            <label htmlFor="email" className={style.email_label}>
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              value={userData.email}
+              onChange={handleChange}
+              className={style.input_field}
+            />
+          </div>
+          <span>{errors.email && errors.email}</span>
 
-      <label htmlFor="password">Password</label>
-      <input
-        type="password"
-        name="password"
-        value={userData.password}
-        onChange={handleChange}
-      />
-      <span>{errors.password && errors.password}</span>
+          <div className={style.input_container}>
+            <label htmlFor="password" className={style.passw_label}>
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              value={userData.password}
+              onChange={handleChange}
+              className={style.input_field}
+            />
+          </div>
+          <span>{errors.password && errors.password}</span>
 
-      <hr />
-      {errors.email || errors.password ? (
-        <div>
-          {}
-          {}
-        </div>
-      ) : null}
-      <button type="submit" disabled={errors.email || errors.password}>
-        Submit
-      </button>
-    </form>
+          {errors.email || errors.password ? (
+            <div>
+              {}
+              {}
+            </div>
+          ) : null}
+          <button className={style.btn_submit} type="submit" disabled={errors.email || errors.password}>
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
